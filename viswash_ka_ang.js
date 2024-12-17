beb2a26c966b0afd179197f142a1e69f6613d269
Shubham Kumar <shubham2270@gmail.com>
2024-12-17 18:19:13 +0530

    added viswash ko ang doha from 16 to 26

---
+++ viswash_ka_ang.js
@@ -0,0 +1,181 @@
+const viswash_eng_16_26 = [
+  {
+    lang: "eng",
+    serial: 16,
+    doha: "पद गावै लौ लीन है, कटै न संसे फाँस\nसबै पछोरै थोथरा, एक बिना विस्वास | | १६ ||",
+    meaning:
+      "Even if one sings God's praises with devotion, without faith, the snare of ignorance will not be cut, for without faith, all practices and devotions are in vain.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 17,
+    doha: "पांडर पिंजर मन भँवर, अरथ अनूपम बास ।\nएक नाम सींची अमी, फल लागा विस्वास | | १७ ||",
+    meaning:
+      "The human body is like a jasmine flower, with good virtues as its fragrance and the mind as the bee. Nourish this flower with the nectar of God's name, and it will bear the divine fruit of faith.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 18,
+    doha: "पीछे चाहे चाकरी, पहिले महिना देय ।\nता साहिब सिर सौंपते, क्यूं कसकाता देह | | १८ ||",
+    meaning:
+      "God provides everything necessary for the body’s sustenance—air, water, sunlight, food—even before birth. Only then does He seek devotion. Why, then, should the body hesitate to surrender to such a loving God?",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 19,
+    doha: "पौ फाटी पगरा भया, जागे सब काहू जीवा जून ।\nको देत हैं, चोंच समाना चून । । १९ ।।",
+    meaning:
+      "As dawn breaks and light spreads, all creatures awaken. God provides each one with food that fits in its beak, so why worry?",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 20,
+    doha: "भक्त भरोसे राम के, निधड़क ऊँची दीठ ।\nतिनकू करम न लागई, राम ठकोरी पीठ ||२०||",
+    meaning:
+      "A devotee, trusting in Ram, lives fearlessly with an elevated outlook. Karma does not trouble him, for Ram stands as his support.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 21,
+    doha: "भजन भरोसे राम के मगहर तजा शरीर।\nअभिनाशी की गोद में, पहुँचै दास कबीर | | २१ ||",
+    meaning:
+      "Kabir says, relying on Ram's devotion, I am shedding my body in Maghar and entering the embrace of the eternal God.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 22,
+    doha: "भूखा भूखा क्या करै, कहा सुनावै लोग।\nभांडा गढ़िया मुख दिया, सोई पूरन जोग | | २२ ||",
+    meaning:
+      "Why does one cry about hunger? Why share your troubles with others? The Creator who shaped the body and gave it a mouth will also provide for its sustenance.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 23,
+    doha: "मुरदे को भी देत है, कपड़ा पानी आग ।\nजीवत नर चिन्ता करै, ताका बड़ा अभाग | | २३ ||",
+    meaning:
+      "God provides even the dead with cloth, water, and fire, yet a living person worries about survival—this is his great misfortune.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 24,
+    doha: "मेरो चित्यो हरि करै, क्या करूं मैं चिंत ।\nहरि को चित्यौ हरि करे, ता पर रहु निचित । । २४ ।।",
+    meaning:
+      "God takes care of my concerns, so why should I worry? I meditate on Him with complete trust, knowing He looks after His devotees.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 25,
+    doha: "रचनहार को चीन्हि ले, खाने को क्या रोय ।\nमन मंदिर में पैठि के, तानि पिछोरी सोय ।। २५ ।।",
+    meaning:
+      "Recognize the Creator; why cry over sustenance? Enter the temple of your mind, meditate on God, cover yourself with the blanket of contentment, and rest assured—God takes care of His devotees.",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "eng",
+    serial: 26,
+    doha: "राखनहारा राम है, जाय जंगल में बैठ ।\nहरि कोपै नहिं ऊबरै सात पताले पैठ | | २६ | |",
+    meaning:
+      "If Ram wishes to protect, even in a remote jungle, He will. But if He is displeased, no one can be saved, even if they hide deep in the netherworld.",
+    ang: "Viswash ko ang",
+  },
+];
+
+const viswash_hin_16_26 = [
+  {
+    lang: "hin",
+    serial: 16,
+    doha: "पद गावै लौ लीन है, कटै न संसे फाँस\nसबै पछोरै थोथरा, एक बिना विस्वास | | १६ ||",
+    meaning:
+      "कोई प्रभुभजन प्रेममग्न होकर गाता हो, तब भी एक प्रभु-विश्वास बिना उसका अज्ञान-फांस नहीं कटता, क्योंकि विश्वास शून्य समस्त साधन-अभ्यास, भजन-ध्यान व्यर्थ है।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 17,
+    doha: "पांडर पिंजर मन भँवर, अरथ अनूपम बास ।\nएक नाम सींची अमी, फल लागा विस्वास | | १७ ||",
+    meaning:
+      "मानव-शरीर कुन्द - फूल के समान है। सद्भाव ही इसकी अनुपम सुगन्ध है और मन भ्रमर है। इस पुष्प को प्रभु के अमृतमय नाम-सुमिरण से सिंचन करो, तब इसमें प्रभु विश्वास का दिव्य फल लगेगा।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 18,
+    doha: "पीछे चाहे चाकरी, पहिले महिना देय ।\nता साहिब सिर सौंपते, क्यूं कसकाता देह | | १८ ||",
+    meaning:
+      "परमात्मा पहले शरीररूपी वेतन एवं शरीरनिर्वाह के लिए आवश्यक सामग्री हवा, पानी, सूर्य, अन्न-जल इत्यादि और जन्म से पूर्व मां के स्तनों में दूध दे देता है। पीछे अपनी सेवा चाहता है। ऐसे प्रेमिल परमात्मा को मन समर्पण करने में देह क्यों डगमगाता है?",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 19,
+    doha: "पौ फाटी पगरा भया, जागे सब काहू जीवा जून ।\nको देत हैं, चोंच समाना चून । । १९ ।।",
+    meaning:
+      "अन्धकार फटा, प्रकाश हुआ और सभी जीव-जन्तु जागे। तब परमात्मा सभी प्राणियों को उसके चोंच में समाने योग्य भोजन देता है, फिर चिन्ता क्यों?",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 20,
+    doha: "भक्त भरोसे राम के, निधड़क ऊँची दीठ ।\nतिनकू करम न लागई, राम ठकोरी पीठ ||२०||",
+    meaning:
+      "भक्त राम के भरोसे निर्भय रहकर ऊंचा देखता है। उसे कर्मफल नहीं सताते, क्योंकि राम उसकी पीठ पर सहारा हैं।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 21,
+    doha: "भजन भरोसे राम के मगहर तजा शरीर।\nअभिनाशी की गोद में, पहुँचै दास कबीर | | २१ ||",
+    meaning:
+      "कबीर साहब कहते हैं, राम के भजन के भरोसे मैं मगहर में शरीर त्याग कर रहा हूं और अविनाशी परमात्मा की गोद में पहुंचने वाला हूं।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 22,
+    doha: "भूखा भूखा क्या करै, कहा सुनावै लोग।\nभांडा गढ़िया मुख दिया, सोई पूरन जोग | | २२ ||",
+    meaning:
+      "भूखा-भूखा क्या चिल्लाता है? अपना दुख लोगों को क्या सुनाता है? जिसने शरीर को गढ़ा और मुख दिया, वही इसकी जीवनरक्षा का उपाय भी करेगा।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 23,
+    doha: "मुरदे को भी देत है, कपड़ा पानी आग ।\nजीवत नर चिन्ता करै, ताका बड़ा अभाग | | २३ ||",
+    meaning:
+      "परमात्मा मुरदे को भी कफन, पानी और आग देता है, पर जीवित मनुष्य जीवननिर्वाह की चिन्ता करता है, यह उसका बहुत बड़ा दुर्भाग्य है।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 24,
+    doha: "मेरो चित्यो हरि करै, क्या करूं मैं चिंत ।\nहरि को चित्यौ हरि करे, ता पर रहु निचित । । २४ ।।",
+    meaning:
+      "मेरी चिन्ता प्रभु करते हैं, तो मैं क्यों चिन्ता करूं? मैं तो प्रभु का ध्यान लगाकर निश्चिन्त रहता हूं, क्योंकि वे भक्तों की चिन्ता करते हैं।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 25,
+    doha: "रचनहार को चीन्हि ले, खाने को क्या रोय ।\nमन मंदिर में पैठि के, तानि पिछोरी सोय ।। २५ ।।",
+    meaning:
+      "सृजनहार परमात्मा को पहचान ले। खाने के लिए क्या रोते हो? मन-मंदिर में प्रभु का ध्यान लगाकर सन्तोषरूपी चादर ओढ़कर निश्चिन्त सो जा, क्योंकि भक्तों का योगक्षेम निश्चित ही प्रभु उठाते हैं।",
+    ang: "Viswash ko ang",
+  },
+  {
+    lang: "hin",
+    serial: 26,
+    doha: "राखनहारा राम है, जाय जंगल में बैठ ।\nहरि कोपै नहिं ऊबरै सात पताले पैठ | | २६ | |",
+    meaning:
+      "यदि राम रक्षा करना चाहता है तो जंगल में जाकर बैठने पर भी रक्षा करेगा और वह किसी कारणवश कुपित है तो सात पाताल के नीचे छुपकर बैठने पर भी कोई नहीं बचा सकता।",
+    ang: "Viswash ko ang",
+  },
+];
